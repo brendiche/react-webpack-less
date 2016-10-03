fde86e1b6d0d17c0a0824198a0fc04b42376f0a2
bgouin <bgouin@astek.fr>
2016-10-03 17:40:45 +0200

    login form sumit value in console.log

--- app/components/login/login.jsx
+++ app/components/login/login.jsx
@@ -10,7 +10,18 @@ class Login extends React.Component {
       password : ""
     };
   }
+  
+  changeLogin(e){
+    this.setState({login:e.target.value});
+  }
 
+  changePassword(e){
+    this.setState({password:e.target.value});
+  }
+  
+  login(){
+    console.log(this.state);
+  }
 
   render() {
     return (
@@ -19,12 +30,13 @@ class Login extends React.Component {
           <from>
             <div className="form-group">
               <label for="login">Login</label>
-              <input type="text" className="form-control" id="login" placeholder="Login" />
+              <input type="text" className="form-control" id="login" placeholder="Login" onChange={this.changeLogin.bind(this)}/>
             </div>
             <div className="form-group">
               <label for="password">Password</label>
-              <input type="password" className="form-control" id="password" placeholder="Password" />
+              <input type="password" className="form-control" id="password" placeholder="Password" onChange={this.changePassword.bind(this)}/>
             </div>
+            <button className="btn" onClick={this.login.bind(this)}>Submit</button>
           </from>
         </div>
       </div>
