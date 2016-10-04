9711c66a083dfc0f0d513ca77be88296dca3eb9b
bgouin <bgouin@astek.fr>
2016-10-04 11:37:02 +0200

    add layout

--- app/components/login/login.jsx
+++ app/components/login/login.jsx
@@ -25,15 +25,15 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div className="container login">
+      <div className="login">
         <div className="jumbotron">
           <from>
             <div className="form-group">
-              <label for="login">Login</label>
+              <label>Login</label>
               <input type="text" className="form-control" id="login" placeholder="Login" onChange={this.changeLogin.bind(this)}/>
             </div>
             <div className="form-group">
-              <label for="password">Password</label>
+              <label>Password</label>
               <input type="password" className="form-control" id="password" placeholder="Password" onChange={this.changePassword.bind(this)}/>
             </div>
             <button className="btn" onClick={this.login.bind(this)}>Submit</button>
--- app/components/router.jsx
+++ app/components/router.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Login } from './login/login.jsx';
+import { Layout } from './layout/layout.jsx';
 import { Router, Route, IndexRoute , hashHistory} from 'react-router';
 
 export default () => {
 	return (
 		<Router history={hashHistory}>
-			<Route path="/">
+			<Route path="/" component={Layout}>
+				<IndexRoute component={Login} />
 				<Route path="login" component={Login} />
+
 			</Route>
 		</Router>
 	);
